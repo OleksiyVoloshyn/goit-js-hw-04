2c1fddd92ee87474c2969f98736a2bb7a745a902
OleksiyGromov <gromov196312@gmail.com>
2024-09-22 16:56:30 +0300

    task3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,22 @@
 'use strict'
+let profile = {
+    username: "Jacob",
+    playtime: 300,
+    changeUsername(newName) {
+        this.username = newName;
+    },
+    updatePlayTime(hours) {
+        this.playtime += hours;
+    },
+    getInfo() {
+        return `${this.username} has ${this.playtime} active hours!`;
+    },
+}
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
